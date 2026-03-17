45772ff0f792f0c5baeef0af689e918f30ffd86a
codegenie-hackabot <codegenie-bot@proton.me>
2026-03-16 17:42:40 -0700

    Make ground more visible (green rectangle)

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const ctx = canvas.getContext('2d');
 // Player object (does not respond to input)
 const player = { x: 100, y: 500, width: 40, height: 60, color: 'red' };
 
-// Simple ground
-const groundY = 560;
+// Simple ground dimensions
+const ground = { y: 560, height: 40, color: 'green' };
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // Draw ground
-  ctx.fillStyle = 'green';
-  ctx.fillRect(0, groundY, canvas.width, canvas.height - groundY);
+  // Draw ground as a green rectangle
+  ctx.fillStyle = ground.color;
+  ctx.fillRect(0, ground.y, canvas.width, ground.height);
   // Draw player (static)
   ctx.fillStyle = player.color;
   ctx.fillRect(player.x, player.y, player.width, player.height);
